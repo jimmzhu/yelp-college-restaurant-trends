e97cbbb9901cf6018baa115c8a0f82dbdc7b85ee
Jimmy Zhu <jimmzhu@gmail.com>
2016-02-27 13:20:39 -0800

    Use snake_case, add parse_businesses, edit stuff

---
+++ src/parse_businesses.py
@@ -0,0 +1,14 @@
+import json
+from peak_check_in import find_peak_ci
+
+def parse_businesses_json(businesses_json):
+    """ returns list of businesses given json file containing json businesses
+    """
+    pass
+
+def businesses_to_csv(businesses):
+    """ takes list of businesses (dicts) and outputs csv file """
+    for business in businesses:
+        business_row = []
+
+
